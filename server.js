7409192eb8058b051684c14bd076245392b97dfd
jsalamanca11 <j.salamanca611@gmail.com>
2017-03-01 22:35:09 -0500

    server.js made

---
+++ server.js
@@ -0,0 +1,94 @@
+//----------------//
+var request = require("request");
+
+var cheerio = require("cheerio");
+
+var express = require("express");
+var mongojs = require("mongojs");
+var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
+//future models for note and story/article
+//var Note = require("./models/Note.js");
+//var Story = require("./models/Story.js");
+
+mongoose.Promise = Promise;
+var app = express();
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost/scraper");
+var db = mongoose.connection;
+
+// errors to be shown
+db.on("error", function(error) {
+    console.log("Mongoose Error: ", error);
+});
+
+// success message
+db.once("open", function() {
+    console.log("Mongoose connection successful.");
+});
+
+//Gets all the articles we scraped from mongoDB
+app.get("/all", function(req, res) {
+    // Finds every doc in the Articles array
+    Article.find({}, function(error, doc) {
+      
+        if (error) {
+            console.log(error);
+        }
+       
+        else {
+            res.json(doc);
+        }
+    });
+});
+
+//Scrape data from one site and place it into the mongodb db
+app.get("/scrape", function(req, res) {
+    // Making a request call for BBC
+    request("http://www.bbc.com/news/world", function(error, response, html) {
+
+        //Loaded the HTML into cheerio and saved it to a variable
+        var $ = cheerio.load(html);
+      
+        $("a.title-link__title-text").each(function(i, element) {
+
+            //Save an empty result object
+            var result = {};
+
+            // Add the text and href of every link, and save them as properties of the result object
+            result.title = $(this).text();
+            result.link = $(this).attr("href");
+
+            // Using our Article model, create a new entry
+            var entry = new Article(result);
+
+            // Now, save that entry to the db
+            entry.save(function(err, doc) {
+            
+                if (err) {
+                    console.log(err);
+                }
+                
+                else {
+                    console.log(doc);
+                }
+            });
+
+
+
+        });
+    });
+    //Sends message of complete
+    res.send("Scrape Complete")
+});
+
+
+// Listen on port 3000
+app.listen(3000, function() {
+    console.log("App running on port 3000!");
+});
